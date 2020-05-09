668a5c584d4e02a3406703bfe12f2f1eef132799
Samuel Frei <samuel.frei@nexiles.com>
2020-05-09 16:23:39 +0200

    Added tracksearch-web module
    - Added module backend with spring boot stuff to tracksearch-web
    - Added placeholder for frontend to tracksearch-web

---
+++ tracksearch-web/backend/src/main/java/io/sfrei/tracksearch_web/TracksearchWebApplication.java
@@ -0,0 +1,13 @@
+package io.sfrei.tracksearch_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracksearchWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracksearchWebApplication.class, args);
+	}
+
+}
